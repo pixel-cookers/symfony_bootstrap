0622770d9a266dd90bfad483bb5269e19b8f13f1
Michel Parpaillon <michel.parpaillon@pixel-cookers.com>
2011-09-29 10:09:41 +0200

    Fix sur btn topbar

--- apps/frontend/modules/sfGuardAuth/templates/_signin.php
+++ apps/frontend/modules/sfGuardAuth/templates/_signin.php
@@ -2,5 +2,5 @@
 	<?php echo $form['_csrf_token'] ;?>
 	<?php echo $form['username']->render(array("placeholder" => "Utilisateur", "class" => "small")) ?>
 	<?php echo $form['password']->render(array("placeholder" => "Mot de passe", "class" => "small")) ?>
-	<input type="submit" class="btn primary" value="Connexion" />
+	<button type="submit" class="btn primary">Connexion</button>
 </form>
